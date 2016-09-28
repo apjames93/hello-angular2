43d5cb240ef162bcde844591fac8fe90c569aff6
Alexander James <apjames93@gmail.com>
2016-09-28 17:32:46 -0600

    set up two way binding data with a input form

--- angular-quickstart/app/app.component.ts
+++ angular-quickstart/app/app.component.ts
@@ -6,12 +6,20 @@ export class Hero {
   name: string;
 }
 
-
-
 @Component({
   selector: 'my-app',
-  template: '<h1>{{title}}</h1> <h2> {{hero.name}} details'
+  template:`
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    `
+
 })
+
 export class AppComponent {
   title = 'Tour of heroes';
   hero: Hero = {
--- angular-quickstart/app/app.module.js
+++ angular-quickstart/app/app.module.js
@@ -11,15 +11,23 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 //this is app entry pointand modules needed to run a angular2 app
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
+var forms_1 = require('@angular/forms');
 var app_component_1 = require('./app.component');
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule],
-            declarations: [app_component_1.AppComponent],
-            bootstrap: [app_component_1.AppComponent]
+            imports: [
+                platform_browser_1.BrowserModule,
+                forms_1.FormsModule
+            ],
+            declarations: [
+                app_component_1.AppComponent
+            ],
+            bootstrap: [
+                app_component_1.AppComponent
+            ]
         }), 
         __metadata('design:paramtypes', [])
     ], AppModule);
